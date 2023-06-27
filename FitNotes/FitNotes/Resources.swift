cd07195b2a341556315a311180ab3460811cb788
Artem Marhaza <artemmrgz@gmail.com>
2023-06-27 11:29:07 +0200

    add app colors

---
+++ FitNotes/FitNotes/Resources.swift
@@ -0,0 +1,21 @@
+//
+//  Resources.swift
+//  FitNotes
+//
+//  Created by Artem Marhaza on 27/06/2023.
+//
+
+import UIKit
+
+enum Resources {
+    enum Color {
+        static let darkBlue = UIColor(red: 34/255, green: 34/255, blue: 59/255, alpha: 1)
+        static let mediumPurple = UIColor(red: 74/255, green: 78/255, blue: 105/255, alpha: 1)
+        static let lavender = UIColor(red: 154/255, green: 140/255, blue: 152/255, alpha: 1)
+        static let rosyBrown = UIColor(red: 201/255, green: 173/255, blue: 167/255, alpha: 1)
+        static let beige = UIColor(red: 242/255, green: 233/255, blue: 228/255, alpha: 1)
+    }
+    
+    static let cornerRadiusCoefficient = 0.17
+    
+}
